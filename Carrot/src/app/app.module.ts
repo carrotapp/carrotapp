08c2cbab166adc2b160f1d75c2ed7b53b66b740f
E-Kaese <ernstkaese@gmail.com>
2017-09-28 09:21:43 +0200

    Created custom components
    Custom components created for buttons and textfields.

--- Carrot/src/app/app.module.ts
+++ Carrot/src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
+import { CustomTextfieldComponent } from './custom-textfield/custom-textfield.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    CustomButtonComponent,
+    CustomTextfieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Carrot/src/app/custom-button/custom-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.css']
+})
+export class CustomButtonComponent implements OnInit {
+
+  @Input()
+  text: String;
+
+  @Input()
+  link: String;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
