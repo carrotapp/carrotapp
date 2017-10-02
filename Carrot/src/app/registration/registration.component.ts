c5e0ab831396e1a4dbc5be655fef24d1fd8af8f7
E-Kaese <ernstkaese@gmail.com>
2017-10-02 14:41:49 +0200

    Added sign-up functionality

--- Carrot/src/app/registration/registration.component.ts
+++ Carrot/src/app/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  database = firebase.auth();
+  email = '';
+  password = '';
+  confirmPassword = '';
 
-  constructor() { }
+  constructor(public af: AngularFireDatabase) { }
 
   ngOnInit() {
   }
 
+  register() {
+    if (this.email !== '' && this.password !== '' && this.confirmPassword !== '') {
+      if (this.password !== this.confirmPassword) {
+        alert('Password do not match');
+      } else {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function (error) {
+          const errorMessage = error.message;
+          const errorCode = error.name;
+          if (errorCode === 'auth/email-already-in-use') {
+            alert('A user with that email already exists');
+          }
+        });
+        alert('Registered successfully!');
+      }
+    } else {
+      alert('Fill out all the fields.');
+    }
+  }
+
+  googlePopup() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+     // This gives you a Google Access Token.
+     const token = result.credential.accessToken;
+     // The signed-in user info.
+     const user = result.user;
+    });
+  }
 }
