3abb9c7157dae321eaa1e660754cebb0f272e400
zahirah <zahirah.ismail1@gmail.com>
2017-10-12 12:49:11 +0200

    routing of expandedInfo

--- Carrot/src/app/app-routing.module.ts
+++ Carrot/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PanelComponent } from './navigation/panel/panel.component';
+import { ExpandedInfoComponent} from './expanded-info/expanded-info.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoreComponent } from './more/more.component';
@@ -10,7 +11,9 @@ const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegistrationComponent },
     { path: 'main', component: PanelComponent},
-    { path: 'main/more', component: MoreComponent}, 
+    { path: 'main/more', component: MoreComponent},
+    { path: 'expandedInfo', component: ExpandedInfoComponent}
+
 ];
 
 @NgModule({
