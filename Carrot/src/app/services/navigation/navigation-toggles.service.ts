c711420ce503ebcc333ae93c7fdd05f4075c1f5e
Sixolile Mtengwana <mtengwanas@gmail.com>
2017-10-17 13:43:27 +0200

    Layout Changes
    Changes to layout.
    + Sidebar Slides in rather than covering the entire screen

--- Carrot/src/app/services/navigation/navigation-toggles.service.ts
+++ Carrot/src/app/services/navigation/navigation-toggles.service.ts
@@ -4,14 +4,20 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class NavigationTogglesService {
-   static ShowSideBar:boolean = false;
-   static togglesidebarstyleClass: String;
+   static ShowSideBar:string;
+   static toolbartoggle: String;
   //Start of Constructor
    constructor() { 
-    NavigationTogglesService.ShowSideBar = false;
-    NavigationTogglesService.togglesidebarstyleClass = "hidden";
+    NavigationTogglesService.ShowSideBar = 'default';
+    NavigationTogglesService.toolbartoggle = "default";
   }
+
+  // Toggles
   sidebar_toggle():void{
-    NavigationTogglesService.ShowSideBar = !NavigationTogglesService.ShowSideBar;
+    if(NavigationTogglesService.ShowSideBar === 'default')
+    NavigationTogglesService.ShowSideBar = 'show';
+  
+    else NavigationTogglesService.ShowSideBar = 'default';
+    console.log(NavigationTogglesService.ShowSideBar);
   }
 }
