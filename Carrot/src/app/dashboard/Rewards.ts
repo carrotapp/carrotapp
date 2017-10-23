9742e533c67f999cea174fbb94bd50c2b1bf5476
Lihle Mdikili <Ltmdikili@gmail.com>
2017-10-23 13:52:38 +0200

    Made changes on the Dashboard
    Moved 90% of the code from the Dashboard component to the service

---
+++ Carrot/src/app/dashboard/Rewards.ts
@@ -0,0 +1,20 @@
+export class Rewards {
+  Currency;
+  Image;
+  RewardName;
+  Name;
+  Ratio;
+  Value;
+  MoreInfo;
+
+  constructor(array, Value) {
+    this.Currency = array[0].$value;
+    this.Image = array[1].$value;
+    this.RewardName = array[2].$value;
+    this.Name = array[3].$value;
+    this.Ratio = array[4].$value;
+    this.Value = Value;
+    this.MoreInfo = array[5].$value;
+  }
+
+}
--- Carrot/src/app/dashboard/dashboard.component.ts
+++ Carrot/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
+import { Rewards } from './Rewards';
 import { DatabaseService } from '../services/database/database.service';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,11 +11,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  userRewards: FirebaseListObservable<any[]>;
-  data: FirebaseListObservable<any[]>;
-  myItems: any[];
-  index = 0;
-
   rewards: Rewards[] = [];
 
   constructor(public afDB: AngularFireDatabase, public afAuth: AngularFireAuth, public router: Router, private ds: DatabaseService) {
@@ -23,55 +19,19 @@ export class DashboardComponent implements OnInit {
       if (afAuth.auth.currentUser.uid === null) {
         console.log('null');
       } else {
-        this.userRewards = ds.getUsersRewards();
-
-        this.userRewards.forEach(element => {
-          const id: String = this.afAuth.auth.currentUser.uid;
-          for (let i = 0; i < element.length; i++) {
-            console.log(element[i].$key);
-            console.log(element[i].$value);
-            this.data = afDB.list('/Rewards/' + element[i].$key);
-            this.data.forEach(dataElement => {
-              this.myItems = dataElement;
-
-              this.rewards.push(
-                new Rewards(this.myItems, element[i].$value)
-              );
-              console.log(this.rewards);
-            });
-        }
-        });
+        this.rewards = ds.getRewardsArray(afAuth.auth.currentUser.uid, afDB);
       }
+
     } catch (error) {
       if (error = 'TypeError: Cannot read property "uid" of null') {
         alert('You are not logged in');
         this.router.navigate(['/']);
       }
     }
-  }
 
+  }
+ 
   ngOnInit() {
   }
 
 }
-
-
-export class Rewards {
-  Currency;
-  Image;
-  RewardName;
-  Name;
-  Ratio;
-  Value;
-
-  constructor(array, Value) {
-    this.Currency = array[0].$value;
-    this.Image = array[1].$value;
-    this.RewardName = array[2].$value;
-    this.Name = array[3].$value;
-    this.Ratio = array[4].$value;
-    this.Value = Value;
-  }
-
-}
-
