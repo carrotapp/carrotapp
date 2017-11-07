ecde1dd57e04a18fd7df4d7aa34e14baf356598d
Sixolile Mtengwana <mtengwanas@gmail.com>
2017-11-07 11:41:33 +0200

    Functional ( Update )
    *Back Button For Regulation Missing

--- Carrot/src/app/navigation/header/header.component.ts
+++ Carrot/src/app/navigation/header/header.component.ts
@@ -10,7 +10,7 @@ export class HeaderComponent {
  /*
    This is a Generic Component that will have titles inserted dynamically
  */
-       @Input('heading') 
+     //  @Input('heading') 
       h1:String;
       // @Input('sub-heading')
        h2:String;
@@ -20,14 +20,15 @@ export class HeaderComponent {
        btn_title :string;
       @Input('showBtn') 
       showBtn:boolean;
-
+      //
+     @Input('username') username:string;
 /* Other Text */
       date:Date;
 
   constructor( private route: ActivatedRoute, private router: Router ) { 
     if(this.btn_title === " "|| this.btn_title === undefined ) this.btn_title = "add more rewards";
    this.showBtn = true;
-   //  this.init();
+  this.init();this.init()
   }
 // Encrypter ?
 pathName(name:string):string{
@@ -42,19 +43,24 @@ capitalize( word:string[] ):string{
 }
 
 //Init
-// init(){
-//   this.route.params.subscribe((params: Params) => {
-//     console.log(this.route);
-//     console.log(this.router.url)
-//     if(this.router.url == '/'+params.username+'/rewards'){
-//     this.h1 = 'My Rewards';
-//     } else {
-//       this.h1 = 'Add Rewards';   
-//     }
-//     this.h2 = params.type;
-//    if( this.h2 == 'view' )
-//     this.showBtn=false;
-//   });
-// }
+init(){
+  this.route.params.subscribe((params: Params) => {
+    console.log(this.route);
+    console.log(this.router.url)
+    console.log(params.username);
+    this.username = params.username;
+    console.log(this.router.url.toString() === '/'+ this.username+'/rewards')
+    if(this.router.url.toString() === '/'+ this.username+'/rewards'){
+    this.h1 = 'Add Rewards';
+  //  this.router.navigate(['/'+ this.username+'/rewards']);
+    } else {
+      this.h1 = 'My Rewards';   
+    // this.router.navigate(['/'+ this.username+'/dashboard']);
+    }
+    this.h2 = params.type;
+   if( this.h2 == 'view' )
+    this.showBtn=false;
+  });
+}
 
 }
