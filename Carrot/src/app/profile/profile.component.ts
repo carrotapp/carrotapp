2b59e7e8f8a166cd6a019f71229b4d6ebb4654e1
Unknown <ernstkaese@gmail.com>
2017-11-07 14:32:55 +0200

    Still working on the profile page

--- Carrot/src/app/profile/profile.component.ts
+++ Carrot/src/app/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { DatabaseService } from './../services/database/database.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected ds: DatabaseService) { }
 
   ngOnInit() {
   }
