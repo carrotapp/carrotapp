cc2607b6f3f23137f3f146f33b60e4311f21c2f5
zahirah <zahirah.ismail1@gmail.com>
2018-01-29 10:19:00 +0200

    implementing of designs

--- Carrot/src/app/services/google/models/gpslocation.service.ts
+++ Carrot/src/app/services/google/models/gpslocation.service.ts
@@ -6,28 +6,28 @@ import { Address } from './address.service';
 
 @Injectable()
 export class GPSLocation {
-  accuracy:number;  
+  accuracy:number;
   location:LocationService;
   address:Address;
- 
-  constructor( accuracy:number, location:LocationService, protected httpRequest:Http) { 
+
+  constructor( accuracy:number, location:LocationService, protected httpRequest:Http) {
     this.accuracy = accuracy;
     this.location = location;
     this.address= new Address( this.location.latitude , this.location.longitude, httpRequest );
     this.createAddress( this.location.longitude , this.location.latitude );
   }
-  
+
   createAddress(lng:number,lat:number):void{
     this.address.createAddress(lng,lat).subscribe(locationTree => {
       /*
       Currently Returns A list of locations who's access points point :nod:
       */
-      // if( Array.isArray( locationTree.results)  && locationTree.results.length > 0   ){ 
+      // if( Array.isArray( locationTree.results)  && locationTree.results.length > 0   ){
         setTimeout(()=>{
         //   for ( let i = 0 ; i < locationTree.results.length ; i++ ){
           /*
            * Needed Attributes to create a legitimate object of Address
-           */          
+           */
                       // let place_id:string = locationTree.results[i].place_id;
                       // let formatted_address:string = locationTree.results[i].formatted_address;
                       // let types:string[] = locationTree.results[i].types;
@@ -36,39 +36,39 @@ export class GPSLocation {
                       // let addressObj:Address=  this.address  = new Address( location.lat , location.lng , this.httpRequest  );
                       // addressObj.createObject( place_id , formatted_address , location_type , types );
                       // this.arrAddress.push(addressObj);
-         /*      
+         /*
           * Filling up the object with its missing data
           * Assign it to the Array of all possible locations
           */
-        
-        // }  
+
+        // }
         let place_id:string = locationTree.results[0].place_id;
         let formatted_address:string = locationTree.results[0].formatted_address;
         let types:string[] = locationTree.results[0].types;
         let location_type:string = locationTree.results[0].geometry.location_type;
         let location = locationTree.results[0].geometry.location;
         let addressObj:Address = new Address( location.lat , location.lng , this.httpRequest  );
-        this.address = addressObj.createObject( place_id , formatted_address , location_type , types ); 
-        console.log(this.address.toString());  
-      } , 1000 );  
+        this.address = addressObj.createObject( place_id , formatted_address , location_type , types );
+        console.log(this.address.toString());
+      } , 1000 );
       // } else if( Array.isArray( locationTree.results ) && locationTree.results.length === 1) {
         /*
          * The Results are always in an array format, if its length is 1, then only one object needs
          * to be created and this will be due to an approximate location feedback.
          */
-    
-        // Instantiates the current address object to eliminat   
+
+        // Instantiates the current address object to eliminat
       // }
-                  
+
     });
   }
   /*
-  accuracy:number;  
+  accuracy:number;
   location:LocationService;
-  address:Address  
+  address:Address
   */
 
-  createGPLocation( location:LocationService , address:Address ):GPSLocation{
+  createGPLocation( location: LocationService , address:Address ):GPSLocation{
     this.location = location;
     this.address = address;
     return this;
@@ -82,4 +82,4 @@ export class GPSLocation {
 
   }
 
-} 
+}
